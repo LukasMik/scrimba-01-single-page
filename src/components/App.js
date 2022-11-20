973c7acbdf7f6ac141fe27d14393b2d9ba568f52
Lukáš Mikuláštík <mikulastik@slajit.cz>
2022-11-20 15:40:02 +0100

    set environment

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,4 @@
 import '../styles/App.scss';
 
-function App() {
-    return(
-        <h1 className='text-center text-2xl text-blue-600'>Hi, I'm heading of App component</h1>
-    )
-}
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './components/App';
+// import App from './components/App';
+import logo from './images/logo.svg'
 import reportWebVitals from './reportWebVitals';
+
+/*
+Challenge: Starting from scratch, build and render the
+HTML for our section project. Check the Google slide for
+what you're trying to build.
+
+We'll be adding styling to it later.
+
+Hints:
+* The React logo is a file in the project tree, so you can
+  access it by using `src="./react-logo.png" in your image
+  element
+* You can also set the `width` attribute of the image element
+  just like in HTML. In the slide, I have it set to 40px
+ */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.Fragment>
-        <App />
-    </React.Fragment>
+    <img src={logo} alt=""/>
 );
 
 // If you want to start measuring performance in your app, pass a function
