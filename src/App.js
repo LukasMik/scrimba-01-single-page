40b5d930a8cfb36b134a070a1a1b50d703499a59
Lukáš Mikuláštík <mikulastik@slajit.cz>
2022-11-20 19:13:04 +0100

    set default environment

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './styles/App.scss';
+
+function App() {
+    return(
+        <h1 className='text-center text-2xl text-blue-600'>You have your default environment set up! Let start new part of course</h1>
+    )
+}
+
+export default App;
