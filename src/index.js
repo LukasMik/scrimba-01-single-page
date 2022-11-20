2d43a13d14cf5e4cf7cd14835c765fec84ba142a
Lukáš Mikuláštík <mikulastik@slajit.cz>
2022-11-20 17:21:39 +0100

    create function component

--- src/index.js
+++ src/index.js
@@ -5,35 +5,37 @@ import './index.scss';
 import logo from './images/logo.svg'
 import reportWebVitals from './reportWebVitals';
 
-/*
-Challenge: Starting from scratch, build and render the
-HTML for our section project. Check the Google slide for
-what you're trying to build.
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-We'll be adding styling to it later.
+/**
+ Challenge:
 
-Hints:
-* The React logo is a file in the project tree, so you can
-  access it by using `src="./react-logo.png" in your image
-  element
-* You can also set the `width` attribute of the image element
-  just like in HTML. In the slide, I have it set to 40px
+ Part 1: Create a page of your own using a custom Page component
+
+ It should return an ordered list with the reasons why you're
+ excited to be learning React :)
+
+ Render your list to the page
+
  */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.Fragment>
-        <img className='w-32 h-auto relative -left-6' src={logo} alt=""/>
-        <h1 className="text-5xl text-blue-200 font-extrabold py-12">Fun facts about React</h1>
-        <ul>
-            <li className="py-2 font-bold text-2xl text-blue-100 ml-8">Was first release in 2013</li>
-            <li className="py-2 font-bold text-2xl text-blue-100 ml-8">Was originally created by Jordan Walke</li>
-            <li className="py-2 font-bold text-2xl text-blue-100 ml-8">Has well over 100k stars on GitHub</li>
-            <li className="py-2 font-bold text-2xl text-blue-100 ml-8">Is mainted by Meta Platforms</li>
-            <li className="py-2 font-bold text-2xl text-blue-100 ml-8">Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </React.Fragment>
-);
+function FunFacts() {
+    return (
+        <React.Fragment>
+            <img className='w-32 h-auto relative -left-6' src={logo} alt=""/>
+            <h1 className="text-5xl text-blue-200 font-extrabold py-12">Fun facts about React</h1>
+            <ul>
+                <li className="py-2 font-bold text-2xl text-blue-100 ml-8">Was first release in 2013</li>
+                <li className="py-2 font-bold text-2xl text-blue-100 ml-8">Was originally created by Jordan Walke</li>
+                <li className="py-2 font-bold text-2xl text-blue-100 ml-8">Has well over 100k stars on GitHub</li>
+                <li className="py-2 font-bold text-2xl text-blue-100 ml-8">Is mainted by Meta Platforms</li>
+                <li className="py-2 font-bold text-2xl text-blue-100 ml-8">Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </React.Fragment>
+    )
+}
+
+root.render(<FunFacts/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
