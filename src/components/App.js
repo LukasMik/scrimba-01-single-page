320acb5e51dd3785e197237da720616533563144
Lukáš Mikuláštík <mikulastik@slajit.cz>
2022-11-20 15:19:16 +0100

    comparing plain el.append() vs reactDOM.createRoot().render

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,3 @@
 import '../styles/App.scss';
 
-function App() {
-  return(
-      <h1>jou</h1>
-  )
-}
-
-/**
- Challenge: find out what happens if we try to append JSX
- to our div#root using .append() instead of ReactDOM
-
- 1. Create a sample page in JSX (≥ 4 elements) and save them in a variable
- 2. Select the div with the ID of "root" and use `.append()` to append
- your JSX
- 3. See if you can guess what will show up in the browser before running
- the code
- 4. See if you can explain what actually shows up in the browser
- */
-
-export default App;
+// export default App;
